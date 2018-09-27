348031763db60a5a265381b704abd634ab2ca970
MichielFeys <43027811+MichielFeys@users.noreply.github.com>
2018-09-27 09:34:43 +0200

    Oppervlakte cirkel

---
+++ 04-Variabelen/Oppervlakte cirkel.py
@@ -0,0 +1,13 @@
+r = float(input(' Geef straal: '))
+
+pi = 3.14159
+
+#bereking
+opp_cirkel = pi * (r ** 2)
+
+
+# uitvoer
+resultaat = 'De oppervlakte van een cirkel met straal '
+resultaat += str(r) + ' is ' + str(opp_cirkel)
+
+print(resultaat)

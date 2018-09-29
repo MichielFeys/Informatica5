cdf79430ffdb6404cdcc3ac0086f8072dfeaf678
MichielFeys <43027811+MichielFeys@users.noreply.github.com>
2018-09-29 11:51:10 +0200

    Avogadro

--- 04-Variabelen/Avogadro.py
+++ 04-Variabelen/Avogadro.py
@@ -1,10 +1,16 @@
-n(x) = float(input(' Geef aantal deeltjes s: '))
+ns = float(input(' Geef aantal deeltjes s: '))
 
 #gegeven
 na = 6.020 * (10 ** 23)
-m(s) = 32.06
-m(x) = m(s)/n
-n(x) = n(s) * n
-#berekening
-print(n(x) + m(x))
+ms = 32.06
+
+#berekeningen
+mx1 = ms/na
+nx = ns * na
+mx2 = mx1 * nx
+#uitvoer
+print(mx2)
+print(mx1 + nx)
+
+
 

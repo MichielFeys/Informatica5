dd876f28db6df6a50a4f9230e548c6366e6290ac
MichielFeys <43027811+MichielFeys@users.noreply.github.com>
2018-09-30 18:58:17 +0200

    Wisselgeld

--- 04-Variabelen/Wisselgeld.py
+++ 04-Variabelen/Wisselgeld.py
@@ -5,13 +5,13 @@ c = bedrag % 100
 aantal_munten += (c // 50)
 c %= 50
 aantal_munten += (c // 20)
-c = c % 20
+c %= 20
 aantal_munten += (c // 10)
-c = c % 10
+c %= 10
 aantal_munten += (c // 5)
-c = c % 5
+c %= 5
 aantal_munten += (c // 2)
-c = c % 2
+c %= 2
 aantal_munten += (c // 1)
 #uitvoer
 print( str(bedrag) + ' cent kan je omwisselen in ' + str(aantal_munten) + ' muntstukken' )

b2267ed7288554b08559dea4595f70bef0dbe2d2
MichielFeys <43027811+MichielFeys@users.noreply.github.com>
2018-10-12 16:39:09 +0200

    Schaal van Saffir-Simpson

---
+++ Condities/Schaal van Saffir-Simpson.py
@@ -0,0 +1,14 @@
+windsnelheid = int(input('Windsnelhied: '))
+
+if windsnelheid >= 119 and windsnelheid <= 153:
+    print('categorie 1')
+if windsnelheid >= 154 and windsnelheid <= 177:
+    print('categorie 2')
+if windsnelheid >= 178 and windsnelheid <= 209:
+    print('categorie 3')
+if windsnelheid >= 210 and windsnelheid <= 249:
+    print('categorie 4')
+if windsnelheid >= 250:
+    print('categorie 5')
+if windsnelheid <= 118:
+    print('geen orkaan')

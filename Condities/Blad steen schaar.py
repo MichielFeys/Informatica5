7148f5b3fe0b5ddf7fc6f0f102effdb6b246c132
MichielFeys <43027811+MichielFeys@users.noreply.github.com>
2018-10-12 16:53:58 +0200

    Blad steen schaar

---
+++ Condities/Blad steen schaar.py
@@ -0,0 +1,12 @@
+speler_1 = input('speler 1: ')
+speler_2 = input('speler 2: ')
+
+if speler_1 == speler_2:
+    print('onbeslist')
+elif speler_1 == 'schaar' and speler_2 == 'blad':
+    print('speler 1 wint')
+elif speler_1 == 'steen' and speler_2 == 'schaar':
+    print('speler 1 wint')
+elif speler_1 == 'blad' and speler_2 == 'steen':
+    print('speler 1 wint')
+else: print('speler 2 wint')

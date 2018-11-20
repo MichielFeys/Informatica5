73fc8843ae67afa711edc1b2fce69f69c5612807
MichielFeys <43027811+MichielFeys@users.noreply.github.com>
2018-11-20 20:16:45 +0100

    Priemgetallen

---
+++ 07b-Iteraties-While-Lus/Priemgetallen.py
@@ -0,0 +1,14 @@
+getal = int(input('getal: '))
+n = 2
+i = 1
+while n < getal:
+    if (getal % n) == 0:
+        antw = '{} is geen priemgetal'.format(getal)
+        i = 0
+    n += 1
+if i and getal != 1:
+    antw = '{} is een priemgetal'.format(getal)
+elif getal == 1:
+    antw = '1 is geen priemgetal'
+
+print(antw)

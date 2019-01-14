e87a01f6300dac0154992034dd2128b143aff092
MichielFeys <43027811+MichielFeys@users.noreply.github.com>
2019-01-14 18:37:28 +0100

    Twitter

---
+++ 10-Strings/Twitter.py
@@ -0,0 +1,8 @@
+tweet = input('tweet: ')
+for i in range(0, len(tweet)- 1):
+    if tweet[i] == '#':
+        na_hashtag = tweet[i::]
+        volgende_spatie= na_hashtag.find(' ')
+        hashtag = na_hashtag[1:volgende_spatie:]
+
+        print(hashtag)

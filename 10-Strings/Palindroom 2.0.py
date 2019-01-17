b889863dcea5df33e84c59bd68e42c1c81a8dd38
MichielFeys <43027811+MichielFeys@users.noreply.github.com>
2019-01-17 08:57:38 +0100

    Palindroom 2.0

---
+++ 10-Strings/Palindroom 2.0.py
@@ -0,0 +1,7 @@
+def is_palindroom(woord):
+    return woord == woord[::-1]
+    #i = 0
+    #while woord[i] == woord[-i - 1] and i < len(woord) // 2:
+    #    i +=1
+
+    #return i == len(woord) // 2

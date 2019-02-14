06d99b874d8888b39df7a67f7dbd9e2430c78555
MichielFeys <43027811+MichielFeys@users.noreply.github.com>
2019-02-14 10:01:54 +0100

    middagmaal

--- 12B-Roosters/Maaltijden.py
+++ 12B-Roosters/Maaltijden.py
@@ -1,6 +1,22 @@
-def dagprijst(bestelling):
-    Middagmaal =  5.3
-    Soep =  1.7
-    Vieruurtje =  2.3
+def maaltijdsprijs(maaltijdtype, aantal):
+    maaltijdprijs = 0
+    if maaltijdtype == 'middagmaal':
+        maaltijdprijs = aantal * 5.3
+    elif maaltijdtype == 'soep':
+        maaltijdprijs = aantal * 1.7
+    elif maaltijdtype == 'vieruurtje':
+        maaltijdprijs = aantal * 2.3
+    return maaltijdprijs
+
+
+def dagprijs(bestelling):
     som = 0
-    for item in bestelling:
+    for i in range(0, len(bestelling), 2):
+        som += maaltijdsprijs(bestelling[i], bestelling[i+1])
+    return som
+
+def weekprijs(bestelling):
+    weekprijs = 0
+    for dag in bestelling:
+        weekprijs += dagprijs(dag)
+    return weekprijs

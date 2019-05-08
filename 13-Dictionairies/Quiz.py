a8db06fe52532b7046c0ab3ecea22a4e222b086c
MichielFeys <43027811+MichielFeys@users.noreply.github.com>
2019-05-08 19:50:24 +0200

    Quiz

--- 13-Dictionairies/Quiz.py
+++ 13-Dictionairies/Quiz.py
@@ -1,8 +1,16 @@
 def verlaat_ploeg(verlater, ploeg, lijst):
+    team = lijst[ploeg]
+    team.remove(verlater)
+    lijst[ploeg] = team
+    if lijst[ploeg] == []:
+        lijst.pop(ploeg)
+    return lijst
 
-    lijst.get(ploeg)
-
-def vervoeg_ploeg(vervoeger, ploeg, lijst):
-
-
+def vervoegt_ploeg(vervoeger, ploeg, lijst):
+    if ploeg in lijst:
+        team = lijst[ploeg]
+        team.append(vervoeger)
+        lijst[ploeg] = team
+    else:
+        lijst[ploeg] = [vervoeger]
     return lijst
